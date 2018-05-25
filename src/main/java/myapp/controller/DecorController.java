41c96b8e5ecd2920e607f4ec6727fe5cbc2da182
cleme <cleme@DESKTOP-QAMR011>
2018-05-25 14:53:08 +0200

    y

--- src/main/java/myapp/controller/DecorController.java
+++ src/main/java/myapp/controller/DecorController.java
@@ -11,10 +11,70 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import myapp.form.DecorForm;
+import myapp.form.ScenarioForm;
 import myapp.model.Decor;
+import myapp.model.Scenario;
 import myapp.repository.DecorRepository;
 
 @Controller
 public class DecorController {
+	
+	@Autowired
+	DecorRepository decorRepository;
+	
+	@GetMapping("decor/decor")
+	public String list(Model model) {
+		model.addAttribute("decorList", decorRepository.findAll());
+		model.addAttribute("decorForm", new DecorForm());
+		return "decor/decor";
+	}
+	
+	@GetMapping("/decor/{id}")
+	public String show(Model model, @PathVariable Long id) {
+			model.addAttribute("decor", decorRepository.findById(id));
+			return "decor/decor";
+	}
+	
+//	@PostMapping("/decor/edit")
+//	public String edit(
+//				@Valid Decor decor,
+//				BindingResult result) {
+//			if (result.hasErrors()) {
+//	            return "decor/decor";
+//	        }
+//			else {
+//	      // Retreive decor d in repository
+//	      Decor d = (decorRepository.findById(decor.getId())).get();
+//	      // Update scenario s:
+//	      		d.setPrice(decor.getPrice());
+//				d.setDescription(decor.getDescription());
+//				d.setName(decor.getName());
+//	      // Save scenario edited:
+//				decorRepository.save(d);
+//	      // Return to the list of decor
+//	      return "redirect:/decor";
+//			}
+//	}
+	
+	@PostMapping("/decor/add")
+	public String decorAdd(
+				@Valid Decor decor,
+				BindingResult result) {
+			if (result.hasErrors()) {
+	            return "decor/decor";
+	        }
+			else {
+	      // Create a new scenario with automatic id generated
+				Decor d = new Decor();
+	      // Update decor d:
+				d.setName(decor.getName());
+				d.setPrice(decor.getPrice());
+				d.setDescription(decor.getDescription());
+	      // Save decor edited:
+				decorRepository.save(d);
+		      // Return to the list of decor
+		      return "redirect:/decor";
+			}
+	}
 
 }
